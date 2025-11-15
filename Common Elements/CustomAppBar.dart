705bcec44d3b04f6d52c5ee4c33ed267df4183d5
PrachiSalunkhe-star <prachisalunkhe877@gmail.com>
2025-11-15 19:25:51 +0530

    Add files via upload
    AppBar is a built-in Flutter widget that displays a toolbar at the top of the screen.
    It usually shows:
    
    Title (text in the center or left)
    
    Menu icon (leading)
    
    Action icons (right side)
    
    Background color
    
    Elevation/shadow
    
    Custom shapes or buttons
    
    The AppBar is commonly placed inside a Scaffold and helps users navigate or perform quick actions.
    
    In this file we create common app bar page for all pages of web apps

---
+++ Common Elements/CustomAppBar.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  const CustomAppBar({super.key,required this.title});
+  
+   @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return  AppBar(
+      title: Text(title),
+      centerTitle: true,
+      backgroundColor: const Color.fromARGB(255, 253, 108, 64),
+      foregroundColor: Colors.black87,
+
+    );
+  }
+ 
+}
