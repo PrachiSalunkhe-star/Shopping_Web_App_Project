6832419470be78ba9cae292057c470bdb894af0a
PrachiSalunkhe-star <prachisalunkhe877@gmail.com>
2025-11-22 22:40:11 +0530

    Add files via upload

---
+++ models/ProductModel.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Product
+{
+  final String name;
+  final String imageurl;
+  final double price;
+
+
+  Product({
+    required this.name,
+    required this.imageurl,
+    required this.price,
+ 
+  });
+  
+}
